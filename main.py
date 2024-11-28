ca4939d65d23146b60db0b06962d0173ceff2178
akki-256 <168257172+akki-256@users.noreply.github.com>
2024-11-28 15:35:07 +0900

    add:AkimotoDIary

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from diaries.DiarySample import DiarySample
+from diaries.akimotoDiary import DiaryAkimoto
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ] 
+diaries = [
+    DiarySample(), 
+    DiaryAkimoto(),
+    ] 
 
 for d in diaries:
     print("---------------------------------")
