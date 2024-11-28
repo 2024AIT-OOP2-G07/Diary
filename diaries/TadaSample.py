5447ed5b2260dcdcbd303ec759071953eb8e8e51
satooru65536 <ryuto4619@gmail.com>
2024-11-28 15:25:02 +0900

    change: 日記を変更

--- diaries/TadaSample.py
+++ diaries/TadaSample.py
@@ -6,7 +6,7 @@ class DiaryTada(AbstractDiary):
         return "2024-11-28"
 
     def get_summary(self):
-        return "GitHub完全に理解した"
+        return "今日は朝起きるのが辛かった.昨夜は労働後にどうしても原神がしたくなって寝るのが遅くなってしまったからだと思う.今日こそは早く寝たい"
 
     def get_author(self):
         return "Tada"
