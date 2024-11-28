65e13623154525277a49818193e5149922fc821c
kanakanho <kahoshibakichi@gmail.com>
2024-11-28 15:23:59 +0900

    feat: 柴田のDiaryの追加

---
+++ diaries/ShibataDiary.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class ShibataDiary(AbstractDiary):
+
+    def get_date(self):
+        return "2024-11-28"
+
+    def get_summary(self):
+        return "Git 学んだ！！"
+
+    def get_author(self):
+        return "柴田青賢"
